ce60d508af4fcac5ea01f858e629174c39acb779
sharv4 <sharvinaangelin04@gmail.com>
2024-11-15 15:20:48 +0530

    created otp verification page

--- frontend/src/pages/resetpassword.js
+++ frontend/src/pages/resetpassword.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "../resetpassword.css";
+import "../styles/resetpassword.css";
 
 const ResetPassword = () => {
   
